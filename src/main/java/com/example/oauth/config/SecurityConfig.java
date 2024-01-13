c976293b4bec1325de2f76f25f569e3d8dc74c5a
Mallika-Dey <mallikaju307@gmail.com>
2024-01-13 12:21:13 +0600

    added jwt converter

--- src/main/java/com/example/oauth/config/SecurityConfig.java
+++ src/main/java/com/example/oauth/config/SecurityConfig.java
@@ -2,7 +2,7 @@ package com.example.oauth.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -10,14 +10,21 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
+    private final JwtAuthConverter jwtAuthConverter;
+
+    public SecurityConfig(JwtAuthConverter jwtAuthConverter) {
+        this.jwtAuthConverter = jwtAuthConverter;
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(request -> request.anyRequest().authenticated())
-                .oauth2ResourceServer(oauth -> oauth.jwt(Customizer.withDefaults()))
+                .oauth2ResourceServer(oauth -> oauth.jwt(
+                        jwt -> jwt.jwtAuthenticationConverter(jwtAuthConverter)))
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .build()
                 ;
--- src/main/java/com/example/oauth/controller/TestController.java
+++ src/main/java/com/example/oauth/controller/TestController.java
@@ -1,12 +1,20 @@
 package com.example.oauth.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
     @GetMapping("/test/api")
+    @PreAuthorize("hasRole('client_user')")
     public String test() {
         return "ok";
+    }
+
+    @GetMapping("/test/api/admin")
+    @PreAuthorize("hasRole('client_admin')")
+    public String test_admin() {
+        return "ok from admin";
     }
 }
